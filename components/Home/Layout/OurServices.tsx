4c00ba0f7b306c4be7a588745474464a2dac22ea
HumptyDumpy01 <tuznikolas@gmail.com>
2024-10-23 19:56:32 +0300

    efactored our services layout onto different reusable components.

---
+++ components/Home/Layout/OurServices.tsx
@@ -0,0 +1,43 @@
+// 'use client';
+
+/*type OurServicesType = {
+  // children: ReactNode;
+}*/
+
+import BuyServiceImg from '@/assets/home/our-services/service-buy.png';
+import RentServiceImg from '@/assets/home/our-services/service-rent.png';
+import SellServiceImg from '@/assets/home/our-services/service-sell.png';
+import CommercialServiceImg from '@/assets/home/our-services/service-commercial.png';
+import React from 'react';
+import CardLink from '@/components/UI/Card/CardLink';
+import HeadingAndLink from '@/components/UI/Link/HeadingAndLink';
+
+export default function OurServices(/*{  }: OurServicesType*/) {
+  return (
+    <>
+      <section className={`mb-20`}>
+        <div className={`max-w-7xl mx-auto w-full overflow-auto`}>
+          <HeadingAndLink
+            heading={`Our Services`}
+            headingSpan={`we can offer a lot!`}
+            linkHref={`/properties`}
+            linkLabel={`Explore`}
+          />
+
+          <div className={`overflow-x-auto`}>
+            <div className={`flex gap-6 w-max`}>
+              <CardLink href={`/properties?type=buy`} heading={`Buy a Property`} metric={1315} metricText={`offers`}
+                        srcImg={BuyServiceImg} altImg={`Buy a Property Image`} />
+              <CardLink href={`/properties?type=rent`} heading={`Rent a Property`} metric={2645} metricText={`offers`}
+                        srcImg={RentServiceImg} altImg={`Rent a Property Image`} />
+              <CardLink href={`/sell`} heading={`Sell a Property`} metric={1} metricText={`offer`}
+                        srcImg={SellServiceImg} altImg={`Sell a Property Image`} />
+              <CardLink href={`/properties?type=commercial`} heading={`Commercial`} metric={611} metricText={`offers`}
+                        srcImg={CommercialServiceImg} altImg={`Commercial Property Image`} />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ components/UI/Card/CardLink.tsx
@@ -0,0 +1,29 @@
+// 'use client';
+
+type CardLinkType = {
+  href: string;
+  srcImg: StaticImageData;
+  altImg: string;
+  heading: string;
+  metric: number;
+  metricText: string;
+  // children: ReactNode;
+}
+
+import Image, { StaticImageData } from 'next/image';
+import React from 'react';
+import Link from 'next/link';
+
+export default function CardLink({ heading, altImg, srcImg, metricText, metric, href }: CardLinkType) {
+  return (
+    <>
+      <Link href={href} className={`flex flex-col`}>
+        <Image className={`mb-6`} src={srcImg} alt={altImg} />
+        <div className={`text-center`}>
+          <h3 className={`text-red-600 font-bold text-3xl mb-3`}>{heading}</h3>
+          <p className={`text-zinc-400 font-medium`}><span>{metric}</span> {metricText}</p>
+        </div>
+      </Link>
+    </>
+  );
+}
