679f33035527242902fab66326f8a367dff51559
HumptyDumpy01 <tuznikolas@gmail.com>
2024-10-24 08:32:24 +0300

    Finished the featured properties section.

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,12 @@ import Hero from '@/components/Layout/Hero/Hero';
 import About from '@/components/Layout/About/About';
 import OurServices from '@/components/Home/Layout/OurServices';
 import HeadingAndLink from '@/components/UI/Link/HeadingAndLink';
+import FeaturedProperty1 from '@/assets/home/featured/featured-1.png';
+import FeaturedProperty2 from '@/assets/home/featured/featured-2.png';
+import FeaturedProperty3 from '@/assets/home/featured/featured-3.png';
+import FeaturedProperty4 from '@/assets/home/featured/featured-4.png';
+import FeaturedProperty5 from '@/assets/home/featured/featured-5.png';
+import CardProperty from '@/components/UI/Card/CardProperty';
 
 export default function Home() {
   return (
@@ -12,13 +18,32 @@ export default function Home() {
       <Hero />
       <About />
       <OurServices />
-      <section>
+      <section className={`mb-20`}>
         <div className={`max-w-7xl mx-auto w-full`}>
           <HeadingAndLink heading={`Featured Properties`} headingSpan={`viaproperty recommends!`} linkLabel={`see all`}
                           linkHref={`/properties?tags=featured`} />
-          <div className={`flex gap-7`}>
-            <div className={`flex flex-col`}>
-            </div>
+          <div className={`flex gap-7 overflow-x-auto overflow-y-hidden pb-8 `}>
+            <CardProperty href={`properties/1`} heading={`Lorem ipsum dolor sit amet, consectetur adipiscing elit...`}
+                          altImg={`A featured property 1`} srcImg={FeaturedProperty1} createdAt={`4`} type={`rent`}
+                          paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna a...`}
+                          total={`5,459`} />
+            <CardProperty href={`properties/2`} heading={`Lorem ipsum dolor sit amet, consectetur adipiscing elit...`}
+                          altImg={`A featured property 2`} srcImg={FeaturedProperty2} createdAt={`7`} type={`buy`}
+                          paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna a...`}
+                          total={`1,459,000`} />
+            <CardProperty href={`properties/3`} heading={`Lorem ipsum dolor sit amet, consectetur adipiscing elit...`}
+                          altImg={`A featured property 3`} srcImg={FeaturedProperty3} createdAt={`2`} type={`rent`}
+                          paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna a...`}
+                          total={`2,459`} />
+            <CardProperty href={`properties/4`} heading={`Lorem ipsum dolor sit amet, consectetur adipiscing elit...`}
+                          altImg={`A featured property 4`} srcImg={FeaturedProperty4} createdAt={`9`} type={`buy`}
+                          paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna a...`}
+                          total={`1,459,000`} />
+            <CardProperty href={`properties/5`} heading={`Lorem ipsum dolor sit amet, consectetur adipiscing elit...`}
+                          altImg={`A featured property 5`} srcImg={FeaturedProperty5} createdAt={`3`} type={`rent`}
+                          paragraph={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna a...`}
+                          total={`3,459`} />
+
           </div>
         </div>
       </section>
---
+++ components/UI/Card/CardProperty.tsx
@@ -0,0 +1,59 @@
+// 'use client';
+
+type CardPropertyType = {
+  type: `rent` | `buy`
+  srcImg: StaticImageData;
+  altImg: string;
+  heading: string;
+  paragraph: string;
+  total: string;
+  createdAt: string;
+  href: string;
+  // children: ReactNode;
+}
+
+import Image, { StaticImageData } from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+export default function
+  CardProperty({
+                 type,
+                 href,
+                 createdAt,
+                 heading,
+                 altImg,
+                 srcImg,
+                 total,
+                 paragraph
+               }: CardPropertyType) {
+  return (
+    <>
+      <div className={`flex flex-col min-w-80`}>
+        <Image src={srcImg} alt={altImg} />
+        <div className={`pl-2 pr-2`}>
+          <div className={`flex flex-col gap-4 mt-7  mb-7`}>
+            <h2 className={`text-xl border-b-blue-950 font-semibold`}>{heading}</h2>
+            <p className={`text-[13px] text-zinc-600`}>{paragraph}</p>
+          </div>
+          <div className={`flex justify-between`}>
+            {type === `rent` && (
+              <>
+                <span className={`text-green-600 inline-block text-[18px] font-semibold`}>${total}<span className={`
+                text-xs`}>/month</span></span>
+              </>
+            )}
+            {type === `buy` && (
+              <>
+                <span className={`text-orange-500 inline-block text-[18px] font-semibold`}>${total}</span>
+              </>
+            )}
+            <p className={`text-xs mb-9`}>{createdAt} days ago</p>
+          </div>
+          <Link className={`text-2xl bg-clip-text text-transparent bg-linear-main-red mb-6
+                font-semibold border-b-4 border-b-red-500 pb-2`} href={href}>See More</Link>
+        </div>
+      </div>
+    </>
+  );
+}
