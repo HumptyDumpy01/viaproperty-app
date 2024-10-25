e6b3b9c21361c9913df89c4339f4dd2b9c8704ad
HumptyDumpy01 <tuznikolas@gmail.com>
2024-10-25 09:42:13 +0300

    Added one more icon - map

--- components/Layout/Form/FormSearch.tsx
+++ components/Layout/Form/FormSearch.tsx
@@ -6,6 +6,7 @@
 
 import SettingsIcon from '@/components/UI/Icon/SettingsIcon';
 import InputSearch from '@/components/UI/Input/InputSearch';
+import MapIcon from '@/components/UI/Icon/MapIcon';
 
 export default function FormSearch(/*{  }: FormSearchType*/) {
   return (
@@ -13,8 +14,9 @@ export default function FormSearch(/*{  }: FormSearchType*/) {
       <form className={`relative flex`}>
         <div className={`flex bp-620:items-center justify-center gap-4 bp-620:gap-11 flex-col bp-620:flex-row`}>
           <InputSearch placeholder={`Country, City, Street`} />
-          <div>
+          <div className={`flex gap-4`}>
             <SettingsIcon />
+            <MapIcon />
           </div>
         </div>
       </form>
