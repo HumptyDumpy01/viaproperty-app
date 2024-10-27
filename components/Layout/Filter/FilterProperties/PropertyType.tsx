12b4c4239e8c206d6133fa998d35eb66738c0f86
HumptyDumpy01 <tuznikolas@gmail.com>
2024-10-27 10:19:39 +0200

    Fixed cosmetic issues with the gap of property type/

--- components/Layout/Filter/FilterProperties/PropertyType.tsx
+++ components/Layout/Filter/FilterProperties/PropertyType.tsx
@@ -11,7 +11,7 @@ export default function PropertyType(/*{  }: PropertyTypeType*/) {
     <>
       <div>
         <h2 className={`text-zinc-900 font-bold text-xl mb-3.5`}>Property Type</h2>
-        <div className={`grid grid-cols-2 gap-y-3.5`}>
+        <div className={`grid grid-cols-2 gap-3.5`}>
           <ButtonPropertyType icon={`home`} label={`Home`} />
           <ButtonPropertyType icon={`apartment`} label={`Apartment`} />
           <ButtonPropertyType icon={`commercial`} label={`Commercial`} />
