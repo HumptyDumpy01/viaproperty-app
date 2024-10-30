a268a3a93615adba68072ece77b4e26e591f7204
HumptyDumpy01 <tuznikolas@gmail.com>
2024-10-30 08:31:17 +0200

    Made the state badge work as intended.

--- app/sell/page.tsx
+++ app/sell/page.tsx
@@ -1,16 +1,48 @@
-// 'use client';
+'use client';
 
 /*type SellPageType = {
   // children: ReactNode;
 }*/
 
+import BadgeStages from '@/components/UI/Badge/BadgeStages';
+import { useState } from 'react';
+
+type activeStateType = {
+  stepOne: `disabled` | `active` | `completed`;
+  stepTwo: `disabled` | `active` | `completed`;
+  stepThree: `disabled` | `active` | `completed`;
+  stepFour: `disabled` | `active` | `completed`;
+  finishingSteps: `disabled` | `active` | `completed`;
+};
+
 export default function SellPage(/*{  }: SellPageType*/) {
+  const [activeState, setActiveState] = useState<activeStateType>({
+    stepOne: `active`,
+    stepTwo: `disabled`,
+    stepThree: `disabled`,
+    stepFour: `disabled`,
+    finishingSteps: `disabled`
+  });
   return (
-    <>
-      <h1>Lorem ipsum dolor.</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor ducimus earum facilis maxime numquam provident
-        quam quis reprehenderit. Adipisci amet consectetur cum delectus id ipsam minus omnis quaerat, quibusdam
-        quidem?</p>
-    </>
+    <main className={`overflow-hidden mt-6`}>
+      <div className={`max-w-[1320px] mx-auto w-full px-3 bp-480:px-6`}>
+        <h1 className={`bg-clip-text text-transparent bg-linear-main-red font-bold
+        text-[40px] mb-3.5 `}>Let’s Start Selling!</h1>
+        <p className={`max-w-4xl leading-relaxed mb-6 text-[15px]`}>Lorem ipsum dolor sit amet, consectetur adipisicing
+          elit. Dolor
+          ducimus earum facilis maxime numquam
+          provident
+          quam quis reprehenderit. Adipisci amet consectetur cum delectus id ipsam minus omnis quaerat, quibusdam
+          quidem?</p>
+        <div className={`flex gap-2.5 items-center`}>
+          <BadgeStages state={activeState.stepOne} label={`Step 1`} object={`stepOne`} />
+          <BadgeStages state={activeState.stepTwo} label={`Step 2`} object={`stepTwo`} />
+          <BadgeStages state={activeState.stepThree} label={`Step 3`} object={`stepThree`} />
+          <BadgeStages state={activeState.stepFour} label={`Step 4`} object={`stepFour`} />
+          <BadgeStages state={activeState.finishingSteps} label={`Finishing Steps`} object={`finishingSteps`} />
+
+        </div>
+      </div>
+    </main>
   );
 }
---
+++ components/UI/Badge/BadgeStages.tsx
@@ -0,0 +1,35 @@
+// 'use client';
+
+type BadgeStagesType = {
+  state: `disabled` | `active` | `completed`;
+  label: string;
+  object: `stepOne` | `stepTwo` | `stepThree` | `stepFour` | `finishingSteps`;
+  // children: ReactNode;
+}
+
+export default function BadgeStages({ state, label }: BadgeStagesType) {
+  const badgeActive = `bg-clip-text text-transparent bg-linear-main-dark-blue font-bold rounded-full border-zinc-200`;
+  const badgeDisabled = `bg-clip-text text-zinc-400 font-medium border-zinc-200`;
+  const badgeComplete = `bg-clip-text text-green-600 font-bold border-green-600`;
+
+  let activeClass = null;
+  switch (state) {
+    case `disabled`:
+      activeClass = badgeDisabled;
+      break;
+    case `active`:
+      activeClass = badgeActive;
+      break;
+    case `completed`:
+      activeClass = badgeComplete;
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <>
+      <span className={`inline-block px-4 py-2 w-fit border rounded-full ${activeClass}`}>{label}</span>
+    </>
+  );
+}
