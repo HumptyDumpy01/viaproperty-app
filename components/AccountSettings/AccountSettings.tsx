b36057c15051844827df65fcd17196e8b2b8cfc8
HumptyDumpy01 <tuznikolas@gmail.com>
2024-11-05 11:27:43 +0200

    Track the subpage on account settings via search params.

--- components/AccountSettings/AccountSettings.tsx
+++ components/AccountSettings/AccountSettings.tsx
@@ -1,22 +1,35 @@
 // 'use client';
 
 import ButtonActive from '@/components/UI/Button/ButtonActive';
-import { ReactNode, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import MyProfile from '@/components/AccountSettings/Settings/MyProfile/MyProfile';
 import MyAdverts from '@/components/AccountSettings/Settings/MyAdverts/MyAdverts';
 import MyWishlist from '@/components/AccountSettings/Settings/MyWishlist/MyWishlist';
 import MyPurchases from '@/components/AccountSettings/Settings/MyPurchases/MyPurchases';
 import DeleteAccount from '@/components/AccountSettings/Settings/DeleteAccount/DeleteAccount';
-
-/*type AccountSettingsType = {
-  // children: ReactNode;
-}*/
+import { useRouter, useSearchParams } from 'next/navigation';
 
 export type ActiveSubPageType = `my-profile` | `my-adverts` | `wishlist` | `my-purchases` | `delete-account`;
 export type ActiveProfilePageType = `overall` | `security`;
 
-export default function AccountSettings(/*{}: AccountSettingsType*/) {
-  const [activeSubPage, setActiveSubPage] = useState<ActiveSubPageType>(`my-profile`);
+export default function AccountSettings() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const subPage = (searchParams.get('subpage') as ActiveSubPageType) || 'my-profile';
+
+  const [activeSubPage, setActiveSubPage] = useState<ActiveSubPageType>(subPage);
+
+  useEffect(() => {
+    if (subPage !== activeSubPage) {
+      setActiveSubPage(subPage);
+    }
+  }, [subPage]);
+
+  useEffect(() => {
+    // @ts-ignore
+    router.push(`/account-settings?page=account-settings&subpage=${activeSubPage}`, undefined, { shallow: true });
+  }, [activeSubPage, router]);
+
   let content: ReactNode;
 
   switch (activeSubPage) {
@@ -28,56 +41,41 @@ export default function AccountSettings(/*{}: AccountSettingsType*/) {
       );
       break;
     case `my-adverts`:
-      content = (
-        <MyAdverts />
-      );
+      content = <MyAdverts />;
       break;
     case `wishlist`:
-      content = (
-        <>
-          <MyWishlist />
-        </>
-      );
+      content = <MyWishlist />;
       break;
     case `my-purchases`:
-      content = (
-        <>
-          <MyPurchases />
-        </>
-      );
+      content = <MyPurchases />;
       break;
     case `delete-account`:
-      content = (
-        <>
-          <DeleteAccount />
-        </>
-      );
+      content = <DeleteAccount />;
+      break;
   }
 
   return (
-    <>
-      <div>
-        <div className={`flex items-center gap-2 overflow-x-auto scrollbar-thin`}>
-          <ButtonActive color={`red`} size={`medium`} onClick={() => setActiveSubPage('my-profile')}
-                        active={activeSubPage === 'my-profile'}
-                        label={`My Profile`} />
-          <ButtonActive color={`red`} size={`medium`} onClick={() => setActiveSubPage('my-adverts')}
-                        active={activeSubPage === 'my-adverts'}
-                        label={`My Adverts`} />
-          <ButtonActive color={`red`} size={`medium`} onClick={() => setActiveSubPage('wishlist')}
-                        active={activeSubPage === 'wishlist'}
-                        label={`Wishlist`} />
-          <ButtonActive color={`red`} size={`medium`} onClick={() => setActiveSubPage('my-purchases')}
-                        active={activeSubPage === 'my-purchases'}
-                        label={`My Purchases`} />
-          <ButtonActive color={`red`} size={`medium`} onClick={() => setActiveSubPage('delete-account')}
-                        active={activeSubPage === 'delete-account'}
-                        label={`Delete Account`} />
-        </div>
-        <div className={`mt-9`}>
-          {content}
-        </div>
+    <div>
+      <div className={`flex items-center gap-2 overflow-x-auto scrollbar-thin`}>
+        <ButtonActive color={`red`} size={`medium`} onClick={() => setActiveSubPage('my-profile')}
+                      active={activeSubPage === 'my-profile'}
+                      label={`My Profile`} />
+        <ButtonActive color={`red`} size={`medium`} onClick={() => setActiveSubPage('my-adverts')}
+                      active={activeSubPage === 'my-adverts'}
+                      label={`My Adverts`} />
+        <ButtonActive color={`red`} size={`medium`} onClick={() => setActiveSubPage('wishlist')}
+                      active={activeSubPage === 'wishlist'}
+                      label={`Wishlist`} />
+        <ButtonActive color={`red`} size={`medium`} onClick={() => setActiveSubPage('my-purchases')}
+                      active={activeSubPage === 'my-purchases'}
+                      label={`My Purchases`} />
+        <ButtonActive color={`red`} size={`medium`} onClick={() => setActiveSubPage('delete-account')}
+                      active={activeSubPage === 'delete-account'}
+                      label={`Delete Account`} />
       </div>
-    </>
+      <div className={`mt-9`}>
+        {content}
+      </div>
+    </div>
   );
 }
