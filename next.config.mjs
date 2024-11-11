abe647576a8140eba767a1b6f24ba4cca7b6516d
HumptyDumpy01 <tuznikolas@gmail.com>
2024-11-11 10:28:02 +0200

    Added cloudinary support in next config.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+// next.config.mjs
+
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: `res.cloudinary.com` }]
+  },
+  env: {
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    NEXT_PUBLIC_CLOUDINARY_API_KEY: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+    NEXT_PUBLIC_CLOUDINARY_API_SECRET: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET
+  }
+};
 
 export default nextConfig;
