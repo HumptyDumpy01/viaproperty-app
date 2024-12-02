f383de1aa4004b204a81f5997ce5a0cc17f2d9d4
HumptyDumpy01 <tuznikolas@gmail.com>
2024-12-02 13:51:48 +0200

    Each time user clicks on pagination btn he scrolls to the top of the page.

--- components/UI/Pagination/Pagination.tsx
+++ components/UI/Pagination/Pagination.tsx
@@ -1,4 +1,5 @@
 import PaginationButton from '@/components/UI/Pagination/PaginationButton';
+import { scrollIntoViewFunc } from '@/utils/functions/scrollIntoViewFunc';
 
 type PaginationType = {
   showing: number;
@@ -14,7 +15,10 @@ export default function Pagination({ showing, total, pages, onPageChange, curren
       <div className={`flex items-center flex-col justify-center mt-14`}>
         <div className={`flex gap-3`}>
           {Array.from({ length: pages }, (_, i) => (
-            <PaginationButton key={i} active={i + 1 === currentPage} page={i + 1} onClick={() => onPageChange(i + 1)} />
+            <PaginationButton key={i} active={i + 1 === currentPage} page={i + 1} onClick={() => {
+              scrollIntoViewFunc(``, true);
+              return onPageChange(i + 1);
+            }} />
           ))}
         </div>
         <p className={`mt-3 text-sm`}>Showing <span>{showing}</span> of <span>{total}</span> results</p>
--- utils/functions/scrollIntoViewFunc.ts
+++ utils/functions/scrollIntoViewFunc.ts
@@ -1,4 +1,12 @@
-export function scrollIntoViewFunc(selector: string) {
+export function scrollIntoViewFunc(selector: string, top?: boolean): void {
+
+  if (top) {
+    // scroll to the top of the screen
+    if (window)
+      window.scrollTo({ top: 0, behavior: `smooth` });
+    return;
+  }
+
   const sellHeading = document.querySelector(`${selector}`);
   if (sellHeading) {
     sellHeading.scrollIntoView({ behavior: `smooth` });
