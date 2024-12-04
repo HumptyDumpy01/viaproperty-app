af33d1b12c9e746e9227da9a716adc161bec9404
HumptyDumpy01 <tuznikolas@gmail.com>
2024-12-04 16:25:31 +0200

    Got rid of redundant code

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -33,11 +33,8 @@ async function fetchProperty(id: string) {
 export default async function PropertyDescription({ params }: { params: { id: string } }) {
   const property = await fetchProperty(params.id);
 
-
-  // const property2 = await fetchProperty(`d989027e-bd7f-45dc-9a8a-9ea36c810989`);
-
   if (!property) return NotFound();
-  console.log(`Executing property: `, property);
+  // console.log(`Executing property: `, property);
 
   return (
     <main className={`mb-24 overflow-hidden`}>
