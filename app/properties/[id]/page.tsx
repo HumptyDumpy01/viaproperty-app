92260ceee553b874732aec474de8be911036bda1
HumptyDumpy01 <tuznikolas@gmail.com>
2024-12-04 20:28:44 +0200

    Used client size parsing for entire property descr page because server side rendering pretty aggressively caches the data. It is not updated.

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,4 +1,5 @@
-// app/properties/[id]/page.tsx
+'use client';
+
 import React from 'react';
 import Accordion from '@/components/Layout/Accordion/AccordionPropertyDescription/Accordion';
 import PropertyGallery from '@/components/PropertyDescription/Layout/PropertyGallery';
@@ -15,13 +16,25 @@ import SidebarContainer from '@/components/PropertyDescription/Layout/SidebarCon
 import ProviderContainer from '@/components/Layout/Provider/ProviderContainer';
 import OpenSidebarBtn from '@/components/PropertyDescription/Layout/OpenSidebarBtn';
 import { GET_PROPERTY } from '@/graphql/property';
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-import NotFound from 'next/dist/client/components/not-found-error';
+import { ApolloClient, DefaultOptions, InMemoryCache } from '@apollo/client';
+import { useFetchProperty } from '@/hooks/useFetchProperty';
+
+const defaultOptions: DefaultOptions = {
+  watchQuery: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'ignore'
+  },
+  query: {
+    fetchPolicy: 'no-cache',
+    errorPolicy: 'all'
+  }
+};
 
 async function fetchProperty(id: string) {
   const apolloClient = new ApolloClient({
     uri: `${process.env.BACKEND_API_URL}/graphql`,
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    defaultOptions: defaultOptions
   });
   const { data } = await apolloClient.query({
     query: GET_PROPERTY,
@@ -30,53 +43,45 @@ async function fetchProperty(id: string) {
   return data.property;
 }
 
-export default async function PropertyDescription({ params }: { params: { id: string } }) {
-  const property = await fetchProperty(params.id);
-
-  if (!property) return NotFound();
-  // console.log(`Executing property: `, property);
+export default function PropertyDescription({ params }: { params: { id: string } }) {
+  const { error, data, loading } = useFetchProperty(params.id);
+  // const property = await fetchProperty(params.id);
+  // if (!property) return NotFound();
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+  const property = data.property;
 
   return (
     <main className={`mb-24 overflow-hidden`}>
       <div className={`max-w-[1320px] mx-auto w-full px-3 bp-480:px-6`}>
         <div className={`grid bp-1306:grid-cols-property-description grid-cols-1 mt-10 gap-5 pr-5 bp-1306:pr-0`}>
           <div>
-            <PropertyGallery
-              title={property.title}
-              images={property.images} />
-            <PropertyTags rating={property.rating.overall}
-                          tags={property.tags} />
-
+            <PropertyGallery title={property.title} images={property.images} />
+            <PropertyTags rating={property.rating.overall} tags={property.tags} />
             <HeadingMedium maxWidthXL={false} customClasses={`mb-8 max-w-screen-xl`} heading={property.title} />
-
             <PropertyConveniences
               additionalConveniences={property.additionalConveniences}
               wifi={true}
               bedrooms={property.propertyHas.bedrooms}
               parkingSpaces={property.propertyHas.parkingSpaces}
-              livingRooms={property.propertyHas.livingRooms} bathrooms={property.propertyHas.bathrooms}
+              livingRooms={property.propertyHas.livingRooms}
+              bathrooms={property.propertyHas.bathrooms}
               beds={property.propertyHas.beds}
               fullKitchen={property.propertyHas.kitchens > 0}
-              sqftSize={property.propertyArea} />
+              sqftSize={property.propertyArea}
+            />
             <Accordion description={property.description} />
-
             <ProviderContainer>
               <OpenSidebarBtn />
             </ProviderContainer>
-
             <div className={`mt-14`}>
-              <HeadingMedium customClasses={`mb-8`} heading={`About
-            Landlord`} />
-
-              <AboutLandlord online={false} initials={`Nikolas Baker`} abbrInitials={`N.B`} text={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-              do
-              eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-              ex
-              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-              fugiat nulla pariatur....`} />
-
-
+              <HeadingMedium customClasses={`mb-8`} heading={`About Landlord`} />
+              <AboutLandlord
+                online={false}
+                initials={`Nikolas Baker`}
+                abbrInitials={`N.B`}
+                text={`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur....`}
+              />
               <RenterReviewsMetrics
                 metrics={{
                   location: 4.44,
@@ -86,28 +91,26 @@ export default async function PropertyDescription({ params }: { params: { id: st
                   amenities: 4.4,
                   security: 3.8
                 }}
-                overallRating={4.3} ratings={534} />
-
+                overallRating={4.3}
+                ratings={534}
+              />
               <div className={`mb-16`}>
                 <PropertyComments />
               </div>
-
               <LeaveCommentContainer />
-
             </div>
           </div>
           <ProviderContainer>
             <SidebarContainer />
           </ProviderContainer>
-
         </div>
       </div>
       <div className={`mt-20`}>
         <CallToActionBlock rounded={false} fullScreen type={`sell-rent`} />
       </div>
       <div className={`ml-3`}>
-        <FeaturedProperties
-          headingLabel={`You might also like...`} headingSpan={`based on your search!`} headingHref={`/properties`} />
+        <FeaturedProperties headingLabel={`You might also like...`} headingSpan={`based on your search!`}
+                            headingHref={`/properties`} />
       </div>
     </main>
   );
