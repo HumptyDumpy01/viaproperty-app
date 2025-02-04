21409479dbadefa126b9c933c205ab47d4a1ea7c
HumptyDumpy01 <tuznikolas@gmail.com>
2025-02-04 13:13:00 +0200

    Added use client to my purchases and my adverts page components.

--- components/AccountSettings/Settings/MyAdverts/MyAdverts.tsx
+++ components/AccountSettings/Settings/MyAdverts/MyAdverts.tsx
@@ -1,4 +1,4 @@
-// 'use client';
+'use client';
 
 /*type MyAdvertsType = {
   // children: ReactNode;
--- components/AccountSettings/Settings/MyPurchases/MyPurchases.tsx
+++ components/AccountSettings/Settings/MyPurchases/MyPurchases.tsx
@@ -1,4 +1,4 @@
-// 'use client';
+'use client';
 
 /*type MyPurchasesType = {
   // children: ReactNode;
