f6d0d90b40a2a89a1763fe3cc1534e14ed5c771f
HumptyDumpy01 <tuznikolas@gmail.com>
2025-02-17 11:04:55 +0200

    changed the copyright for sell page.

--- components/Sell/SellHeader.tsx
+++ components/Sell/SellHeader.tsx
@@ -8,13 +8,10 @@ export default function SellHeader(/*{  }: SellHeaderType*/) {
   return (
     <>
       <h1 className={`bg-clip-text text-transparent bg-linear-main-red font-bold
-        text-[40px] mb-3.5 sell-heading`}>Let’s Start Selling!</h1>
-      <p className={`max-w-4xl leading-relaxed mb-6 text-[15px]`}>Lorem ipsum dolor sit amet, consectetur adipisicing
-        elit. Dolor
-        ducimus earum facilis maxime numquam
-        provident
-        quam quis reprehenderit. Adipisci amet consectetur cum delectus id ipsam minus omnis quaerat, quibusdam
-        quidem?</p>
+        text-[40px] mb-3.5 sell-heading max-w-5xl`}>Your Property, Your Terms – Start Selling or Renting with Ease!</h1>
+      <p className={`max-w-3xl leading-relaxed mb-6 text-[15px]`}>Selling or renting doesn’t have to be a complicated
+        process. With the right approach, you can showcase your property’s best features, set competitive pricing, and
+        attract interest from the right audience.</p>
     </>
   );
 }
