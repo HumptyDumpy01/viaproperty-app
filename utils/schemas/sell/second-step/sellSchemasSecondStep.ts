64f5751bcbb5065e3b927673c84c71a71a7c6dce
HumptyDumpy01 <tuznikolas@gmail.com>
2025-02-24 18:59:18 +0200

    Trim the tags added to new advert in /sell page - fixed the bug with adding the same tag

--- utils/schemas/sell/second-step/sellSchemasSecondStep.ts
+++ utils/schemas/sell/second-step/sellSchemasSecondStep.ts
@@ -1,15 +1,15 @@
 import { z } from 'zod';
 
 export const tagSchema = z.object({
-  tag: z.string().min(1, `Please provide a tag between 1 to 100 characters.`).max(100, `Please provide a tag between 1 to 100 characters.`)
+  tag: z.string().trim().min(1, `Please provide a tag between 1 to 100 characters.`).max(100, `Please provide a tag between 1 to 100 characters.`)
 });
 
 export const tagsSchema = z.object({
-  tags: z.array(z.string()).max(10)
+  tags: z.array(z.string().trim()).max(10)
 }).refine(({ tags }) => new Set(tags).size === tags.length, { message: `Each tag must be unique.` });
 
 export const discountSchema = z.object({
-  property: z.string().min(1).max(3)
+  property: z.string().trim().min(1).max(3)
 }).refine(({ property: discount }) => {
   return !isNaN(Number(discount)) && Number(discount) >= 1 && Number(discount) <= 100;
 });
